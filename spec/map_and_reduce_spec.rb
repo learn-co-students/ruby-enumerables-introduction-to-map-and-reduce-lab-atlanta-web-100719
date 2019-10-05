408527a412cae0c7e8f29002af56a0e73414d75f
Javier Huerta Jr <javsteratii@gmail.com>
2019-10-05 22:01:10 +0000

    Done.

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -14,7 +14,7 @@ describe 'my own map-like methods' do
     end
   end
 
-  describe "map_to_double returns an array with the original values multiplied by 2" do
+  describe "map_to_double returns an array with the original  values multiplied by 2" do
     it "transforms correctly" do
       expect(map_to_double([1, 2, 3, -9])).to eq([2, 4, 6, -18])
     end
